3bc761c1d86d250d4260602af53a4d3d98a20d64
Angelo Barrientos <anbtbarrientos11@gmail.com>
2024-12-14 20:03:24 -0300

    Cambios en twitter backend

--- temporal-backend/src/persintence/models/Twitter.js
+++ temporal-backend/src/persintence/models/Twitter.js
@@ -2,23 +2,24 @@ import { DataTypes } from "sequelize";
 import { sequelize } from "../database/database.js";
 
 export const Twitter = sequelize.define(
-    "twitter",
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: false,
-        },
-        nombre: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        token: {
-            type: DataTypes.BIGINT,
-            allowNull: false,
-        },
+  "twitter",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        tableName: 'twitter', //Nombre de la tabla aqui, ya que si no existe esta linea la convierte a plural
-    }
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    token: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "twitter", //Nombre de la tabla aqui, ya que si no existe esta linea la convierte a plural
+    timestamps: false,
+  }
 );
